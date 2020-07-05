ec8a90a7bfe260a356eb29ef64a5874b2672ef79
Angel-Perdomo <pastorangel1880@gmail.com>
2020-07-04 20:57:56 -0500

    ejercicio 2

---
+++ EJERCICIOS/Ejercicio-2/mensajes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    string nombre;
+    cin >> nombre;
+
+    cout << "Ingrese su nombre: ";
+    cout << endl;
+    cout << "Hola mi nombre es " << nombre;
+    cout << endl;
+
+
+    return 0;
+}
