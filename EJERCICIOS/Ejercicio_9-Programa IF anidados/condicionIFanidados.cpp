60b02248ba500130850dc013259b9c2e41a47453
Angel-Perdomo <pastorangel1880@gmail.com>
2020-07-08 19:24:29 -0500

    Ejercicios de clase

---
+++ EJERCICIOS/Ejercicio_9-Programa IF anidados/condicionIFanidados.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a = 0, b = 0, c = 0;
+
+    cout << endl;
+    cout << "Ingrese a: ";
+    cin >> a;
+    cout << endl;
+
+    cout << "Ingrese b: ";
+    cin >> b;
+    cout << endl;
+
+    cout << "Ingrese c: ";
+    cin >> c;
+    cout << endl;
+
+
+    cout << "---------------------------------";
+    cout << endl;
+    
+    if (a > b)
+    {
+        if (a > c)
+        {
+            cout << "A es mayor" << endl;
+        } else
+        {
+            cout << "C es mayor" << endl;
+        }  
+        
+    } else
+    {
+        if (b > c)
+        {
+            cout << "B es mayor" << endl;
+        } else
+        {
+            cout << "C es mayor" << endl;
+        } 
+    }
+    cout << "---------------------------------";
+    
+
+
+    return 0;
+}
