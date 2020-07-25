412abcbaeab420c6e68c448fd028b26cf4f4ae8d
Angel-Perdomo <pastorangel1880@gmail.com>
2020-07-25 08:51:34 -0600

    Examen I

---
+++ Examen-I-Parcial/examenUniduno.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+using namespace std;
+ 
+
+int Modulus(int iN, int iMod) {
+    int iQ = (iN/iMod);
+    return iN - (iQ*iMod);
+}
+ 
+
+char GetChar(int iGenerator, char cBase, int iRange) {
+    return (cBase + Modulus(iGenerator, iRange));
+}
+ 
+
+int main() {
+    string contrasena;
+    
+    cout << "Eres un agente? ingresa tu password para acceder a la matrix: ";
+    cin >> contrasena;
+    
+    if (contrasena == "smith") {
+        system("COLOR 02");
+ 
+
+        char caRow[80];
+        int j = 7;
+        int k = 2;
+        int l = 5;
+        int m = 1;
+        int ciclos = 0;
+ 
+
+        while (true) {
+            int i = 0;
+            
+            // caracteres aleatorios
+            while (i < 80) {
+                if (caRow[i] != ' ') {
+                    caRow[i] = GetChar(j + i*i, 33, 30);
+                    cout << caRow[i];
+                }
+                
+                ++i;
+            }
+            j = (j + 31);
+            k = (k + 17);
+            l = (l + 47);
+            m = (m + 67);
+            caRow[Modulus(j, 80)] = '-';
+            caRow[Modulus(k, 80)] = ' ';
+            caRow[Modulus(l, 80)] = '-';
+            caRow[Modulus(m, 80)] = ' ';
+            
+            i = 0;
+            while (i < 3000) {
+                GetChar(1, 1, 1);
+                 ++i;
+            }
+ 
+            cout << "*** EL SISTEMA FALLO ***";
+            ciclos++;
+        }       
+ 
+        
+    } else if (contrasena == "neo")
+    {
+        
+
+        char caRow[80];
+        int j = 7;
+        int k = 2;
+        int l = 5;
+        int m = 1;
+        int ciclos = 0;
+ 
+
+        while (ciclos < 11) {
+            system("COLOR 04");
+            int i = 0;
+            
+            // caracteres aleatorios
+            while (i < 80) {
+                if (caRow[i] != ' ') {
+                    caRow[i] = GetChar(j + i*i, 33, 30);
+                    cout << caRow[i];
+                }
+                
+                ++i;
+            }
+            j = (j + 31);
+            k = (k + 17);
+            l = (l + 47);
+            m = (m + 67);
+            caRow[Modulus(j, 80)] = '-';
+            caRow[Modulus(k, 80)] = ' ';
+            caRow[Modulus(l, 80)] = '-';
+            caRow[Modulus(m, 80)] = ' ';
+            
+            i = 0;
+            while (i < 3000) {
+                GetChar(1, 1, 1);
+                 ++i;
+            }
+            
+            cout << "*** EL SISTEMA FALLO***";
+            ciclos++;
+        }
+        
+    } else
+    {
+        cout << endl;
+        cout << "acceso a la matrix denegado";
+        cout << endl;
+    }
+    
+     
+ 
+
+    return 0;
+}
