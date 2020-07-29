c7f7407298122d47241511a8c6542bc98bd2cf7c
Angel-Perdomo <pastorangel1880@gmail.com>
2020-07-29 00:11:18 -0600

    segundo parcial

---
+++ EJERCICIOS/Ejercicio23-Prog Funciones Int (enteras)/progfuncionesint.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int sumar( int a, int b){
+
+    return a + b;
+}
+
+        //ESTOS SON PARAMETROS, DENTRO DEL Main.
+int main(int argc, char const *argv[])
+{
+    system("cls");
+
+    cout << sumar(5, 7);
+    cout << endl;
+
+    return 0;
+}
