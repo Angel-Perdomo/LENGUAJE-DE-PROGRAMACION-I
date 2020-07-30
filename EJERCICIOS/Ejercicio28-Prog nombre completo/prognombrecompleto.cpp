25be85d11c14b779495225483ef52befcd8f634a
Angel-Perdomo <pastorangel1880@gmail.com>
2020-07-30 02:26:49 -0600

    segundo parcial

---
+++ EJERCICIOS/Ejercicio28-Prog nombre completo/prognombrecompleto.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+string nombreCompleto(string nombre, string apellido)
+{
+    return nombre + " " + apellido;
+}
+
+int main(int argc, char const *argv[])
+{
+    string primerNombre = "";
+    string primerApellido = "";
+
+    cout << "Ingrese su primer nombre: ";
+    cin >> primerNombre;
+    cout << endl;
+    cout << "Ingrese su primer apellido: ";
+    cin >> primerApellido;
+    cout << endl;
+
+    cout << "Nombre completo: " << nombreCompleto(primerNombre, primerApellido);
+    cout << endl;
+
+    system("pause");
+    return 0;
+}
