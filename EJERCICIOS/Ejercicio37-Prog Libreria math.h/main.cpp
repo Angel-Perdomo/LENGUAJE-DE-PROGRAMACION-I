f8c8021db3c61ad8fdf33eeff4d78ea969cba782
Angel-Perdomo <pastorangel1880@gmail.com>
2020-08-06 02:57:33 -0600

    Segundo parcial

---
+++ EJERCICIOS/Ejercicio37-Prog Libreria math.h/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <math.h>
+#define Pi 3.1416
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    double numero = 0;
+    double seno = 0; 
+    double coseno = 0; 
+    double tangente = 0;
+
+    numero = 2*Pi;
+    seno = sin(numero); 
+    coseno = cos(numero); 
+    tangente = tan(numero); 
+
+    cout << endl;
+    cout << "Numero: " << numero;
+    cout << endl;
+    cout << "Seno: " << seno;
+    cout << endl;
+    cout << "Coseno: " << coseno;
+    cout << endl;
+    cout << "Tangente: " << tangente;
+    cout << endl;
+
+    system("pause");
+    return 0;
+}
